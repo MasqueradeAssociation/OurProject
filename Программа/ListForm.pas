246fd8014cef015b7ef51c3ae1d3f39df2ad7cdf
Goncharov Cyril <Krio05@yandex.ua>
2018-02-01 22:12:15 +0200

    В пи*ду

--- Программа/ListForm.pas
+++ Программа/ListForm.pas
@@ -8,73 +8,7 @@ uses
 
 type
   TListCharacter = class(TForm)
-    ListImage: TImage;
-    StrengthMP: TImage;
-    AgilityMP: TImage;
-    Image1: TImage;
-    EnduranceMP: TImage;
-    Image2: TImage;
-    Image3: TImage;
-    Image4: TImage;
-    Image5: TImage;
-    Image6: TImage;
-    Image7: TImage;
-    Image8: TImage;
-    Image9: TImage;
-    Image10: TImage;
-    Image11: TImage;
-    Image12: TImage;
-    Image13: TImage;
-    Image14: TImage;
-    Image15: TImage;
-    Image16: TImage;
-    Image17: TImage;
-    Image18: TImage;
-    Image19: TImage;
-    Image20: TImage;
-    Image21: TImage;
-    Image22: TImage;
-    Image23: TImage;
-    Image24: TImage;
-    Image25: TImage;
-    Image26: TImage;
-    Image27: TImage;
-    Image28: TImage;
-    Image29: TImage;
-    Image30: TImage;
-    Image31: TImage;
-    Image32: TImage;
-    Image33: TImage;
-    Image34: TImage;
-    Image35: TImage;
-    Image36: TImage;
-    Image37: TImage;
-    Image38: TImage;
-    Image39: TImage;
-    Image40: TImage;
-    Image41: TImage;
-    Image42: TImage;
-    Image43: TImage;
-    Image44: TImage;
-    Image45: TImage;
-    Image46: TImage;
-    Image47: TImage;
-    Image48: TImage;
-    Image49: TImage;
-    Image50: TImage;
-    Image51: TImage;
-    Image52: TImage;
-    Image53: TImage;
-    Image54: TImage;
-    Image55: TImage;
-    Image56: TImage;
-    Image57: TImage;
-    procedure ListImageMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
-    procedure StrengthMPMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
-    procedure AgilityMPMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+    TitleImg: TImage;
   private
     { Private declarations }
   public
@@ -92,36 +26,8 @@ uses UnitClassList;
 
 {$R *.dfm}
 
-procedure TListCharacter.ListImageMouseDown(Sender: TObject;
-  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  ListCharacter.Caption:='X='+inttostr(X)+'  Y='+inttostr(Y);
-end;
 
-procedure TListCharacter.StrengthMPMouseDown(Sender: TObject;
-  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  if(X <15) then
-  begin
-    ShowMessage('s-');
-  end
-  else
-  begin
-    ShowMessage('s+');
-  end;
-end;
 
-procedure TListCharacter.AgilityMPMouseDown(Sender: TObject;
-  Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  if(X <15) then
-  begin
-    ShowMessage('a-');
-  end
-  else
-  begin
-    ShowMessage('a+');
-  end;
-end;
+
 
 end.
