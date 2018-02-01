af1d4ba56d791a34f01035d41b7773d86dc7440d
Vsevolod <31592913+Vsevood@users.noreply.github.com>
2018-02-01 21:18:54 +0300

    Пол класса персонаж

--- Программа/UnitClassList.pas
+++ Программа/UnitClassList.pas
@@ -2,6 +2,89 @@ unit UnitClassList;
 
 interface
 
+uses classes, Sysutils;
+
+type
+  TList = class
+  public
+    //constructor Create;
+
+    ////Основнвя информация
+    NameCharacter:string;//Имя
+    Owner:string;//Игрок
+    Nature:string;//Натура
+    Mask:string;//Маска
+    Chronicle:string;//Хроника
+    Concept:string;//Концепт
+    Home:string;//Убежище
+    Gen:Integer;//Поколение
+    /////Атрибуты
+    Strength:Integer;//Сила
+    Agility:Integer;//Ловкость
+    Endurance:Integer;//Выносливасть
+    Charm:Integer;//Обаяние
+    Manipulation:Integer;//Манипулирование
+    Appearance:Integer;//Внешность
+    Perception:Integer;//Восприятие
+    Intelligence:Integer;//Интелект
+    Savvy:Integer;//Смекалка
+    ///Способности
+    //Таланты
+    Athletics:Integer;//Атлетика
+    Mindfulness:Integer;//Внимательность
+    Intimidation:Integer;//Запугивание
+    SleightOfHand:Integer;//Ловкость рук
+    Leadership:Integer;//Лидерство
+    Fight:Integer;//Рукопашный бой
+    Evasion:Integer;//Уклонение
+    Cunning:Integer;//Хитрость
+    Expression:Integer;//Экспрессия
+    Empathy:Integer;//Эмпатия
+    //Навыки
+    Driving:Integer;//Вождение, Верховая езда
+    Trade:Integer;//Торговля
+    Survival:Integer;//Выживание
+    Performance:Integer;//Исполнение
+    AnimalKen:Integer;//Знание Животных
+    Handicrafts:Integer;//Ремёсла
+    Evasion:Integer;//Уклонение
+    Cunning:Integer;//Хитрость
+    Expression:Integer;//Экспрессия
+    Empathy:Integer;//Эмпатия
+    //Познания
+    Athletics:Integer;//Атлетика
+    Mindfulness:Integer;//Внимательность
+    Intimidation:Integer;//Запугивание
+    SleightOfHand:Integer;//Ловкость рук
+    Leadership:Integer;//Лидерство
+    Fight:Integer;//Рукопашный бой
+    Evasion:Integer;//Уклонение
+    Cunning:Integer;//Хитрость
+    Expression:Integer;//Экспрессия
+    Empathy:Integer;//Эмпатия
+    ////Преимущества
+    DisciplineName:array [0..20] of string;//Названия дисциплин
+    DisciplineLevel:array [0..20] of Integer;//Уровень дисциплин
+    AdditionsName:array [0..20] of string;//Названия дополнений
+    AdditionsLevel:array [0..20] of Integer;//Уровень дополнений
+    ConscientiousnessAndConviction:Integer;//Сознательность и Убеждённость
+    SelfmonitoringAndInstincts:Integer;//Самоконтроль и Инстинкты
+    Courage:Integer;//Смелость
+    ////Остальная информация
+    HumanityAndPathName:string;//Человечность и Путь
+    HumanityAndPathLevel:Integer;//Человечность и Путь Уровень
+    WillpowerStatic:Integer;//Постоянная Сила воли
+    WillpowerDinamic:Integer;//Временная Сила воли
+    BloodPool:Integer;//Запас крови
+    Virtues:array [0..20] of string;//Добродетели
+    Vices:array [0..20] of string;//Пороки
+    DopStatName:array [0..20] of string;//Названия дополнительных характристик
+    DopStatLevel:array [0..20] of Integer;//Уровни дополнительных характристик
+    XP:array [0..30] of Integer;//ХП
+  end;
+
 implementation
+
+//
 
 end.
